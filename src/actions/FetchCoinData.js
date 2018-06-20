971eab4a6e60c734b32d59703655d938d5a6983d
Karthik Gudipati <karthikgudipati@ravis-mbp.attlocal.net>
2018-06-20 16:43:19 -0700

    Added Actiontypes file and update fetch data reducer

--- src/actions/FetchCoinData.js
+++ src/actions/FetchCoinData.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
 import { API_BASE_URL } from '../Utils/Constants'
 
+import {
+    FETCH_COIN_DATA,
+    FETCH_COIN_DATA_SUCCESS,
+    FETCH_COIN_DATA_FAIL
+} from '../Utils/ActionTypes'
+
 export default function FetchCoinData() {
     return dispatch => {
         dispatch({ type: FETCH_COIN_DATA })
 
-        return axios.get(`${API_BASE_URL}/v1/ticker.?limit=10`)
+        return axios.get(`${API_BASE_URL}/v1/ticker/?limit=10`)
             .then(res => {
                 dispatch({ type: FETCH_COIN_DATA_SUCCESS, payload: res.data })
             })
--- src/reducers/CryptoReducer.js
+++ src/reducers/CryptoReducer.js
@@ -1,3 +1,41 @@
+import {
+    FETCH_COIN_DATA,
+    FETCH_COIN_DATA_SUCCESS,
+    FETCH_COIN_DATA_FAIL
+} from './../Utils/ActionTypes'
+
+const initialState = {
+    isFetching: null,
+    data: [],
+    hasError: false,
+    errorMessage: null
+}
+
 export default function (state = [], action) {
-    return state
+    
+    switch(action.type) {
+
+        case FETCH_COIN_DATA:
+            return { 
+                ...state,
+                isFetching: true,
+                data: null,
+                hasError: false,
+                errorMessage: null
+            }
+
+        case: FETCH_COIN_DATA_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                data: action.payload,
+                hasError: false,
+                errorMessage: null
+            }
+
+        default:
+            return state
+
+    }
+    
 }
